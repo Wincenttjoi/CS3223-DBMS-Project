3eedf1a4d77e03a687a19f05f7d050df2493e9cb
Geonsik Moon <gsmoon97@gmail.com>
2022-03-16 02:23:54 +0800

    Tentatively implement hashjoin

--- SimpleDBEngine/src/simpledb/materialize/HashJoinPlan.java
+++ SimpleDBEngine/src/simpledb/materialize/HashJoinPlan.java
@@ -5,18 +5,41 @@ import simpledb.plan.Plan;
 import simpledb.query.*;
 import simpledb.record.*;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 /**
  * The Plan class for the <i>hashjoin</i> operator.
  * 
- * @author Moon Geonsik
  */
 public class HashJoinPlan implements Plan {
 	private Transaction tx;
 	private Plan p1, p2;
 	private String fldname1, fldname2;
-	private Schema schema = new Schema();
+	private int k;
+	private Schema sch1 = new Schema();
+	private Schema sch2 = new Schema();
+	private Schema finalSch = new Schema();
 	private String opr;
-	private int numPart;
+	private HashComparator comp;
+
+//	 * Let T1 and T2 be the tables to be joined.
+//	 1. Choose a value k that is less than the number of available buffers.
+//	 2. If the size of T2 is no more than k blocks, then:
+//	 a) Join T1 and T2, using a multibuffer product followed by a selection on the join predicate.
+//	 b) Return. // Otherwise:
+//	 3. Choose a hash function that returns a value between 0 and k-1.
+//	 4. For the table T1:
+//	 a) Open a scan for k temporary tables.
+//	 b) For each record of T1:
+//	 i. Hash the record’s join field, to get the hash value h.
+//	 ii. Copy the record to the hth temporary table.
+//	 b) Close the temporary table scans.
+//	 5. Repeat Step 4 for the table T2.
+//	 6. For each i between 0 and k-1:
+//	 a) Let Vi be the ith temporary table of T1.
+//	 b) Let Wi be the ith temporary table of T2.
+//	 c) Recursively perform the hashjoin of Vi and Wi.
 
 	/**
 	 * Creates a hashjoin plan for the two specified queries.
@@ -27,30 +50,37 @@ public class HashJoinPlan implements Plan {
 	 * @param fldname1 the LHS join field
 	 * @param fldname2 the RHS join field
 	 * @param opr      the relational operator between join fields
-	 * @param numPart  the number of partitions
+	 * @param k        the number of partitions
 	 */
-	public HashJoinPlan(Transaction tx, Plan p1, Plan p2, String fldname1, String fldname2, String opr, int numPart) {
+	public HashJoinPlan(Transaction tx, Plan p1, Plan p2, String fldname1, String fldname2, String opr, int k) {
 		this.tx = tx;
 		this.p1 = p1;
 		this.p2 = p2;
 		this.fldname1 = fldname1;
 		this.fldname2 = fldname2;
+		this.k = k;
 		this.opr = opr;
-		this.numPart = numPart;
-		schema.addAll(p1.schema());
-		schema.addAll(p2.schema());
+		this.comp = new HashComparator(fldname1, fldname2, k);
+		sch1 = p1.schema();
+		sch2 = p2.schema();
+		finalSch.addAll(p1.schema());
+		finalSch.addAll(p2.schema());
 	}
 
 	/**
-	 * Creates a hash join scan for this query.
+	 * Creates a hashjoin scan for this query. In the partition phase, k number of
+	 * partitions (temporary tables) are created, and are passed into HashJoinScan
+	 * for joining phase.
 	 * 
 	 * @see simpledb.plan.Plan#open()
 	 */
 	public Scan open() {
 		Scan s1 = p1.open();
 		Scan s2 = p2.open();
+		Map<Integer, TempTable> part1 = splitIntoPartitions(s1, fldname1, sch1);
+		Map<Integer, TempTable> part2 = splitIntoPartitions(s2, fldname2, sch2);
 		printPlan();
-		return new HashJoinScan(s1, s2, fldname1, fldname2, opr, numPart);
+		return new HashJoinScan(s1, s2, fldname1, fldname2, part1, part2, k, opr);
 	}
 
 	/**
@@ -100,13 +130,49 @@ public class HashJoinPlan implements Plan {
 	 * @see simpledb.plan.Plan#schema()
 	 */
 	public Schema schema() {
-		return schema;
+		return finalSch;
+	}
+
+	private Map<Integer, TempTable> splitIntoPartitions(Scan src, String fldname, Schema sch) {
+		Map<Integer, TempTable> temps = initializePartitions(tx, sch, k);
+		src.beforeFirst();
+		if (!src.next())
+			return temps;
+		Constant val = src.getVal(fldname);
+		int offset = val.hashCode() % k;
+		TempTable currenttemp = temps.get(offset);
+		UpdateScan currentscan = currenttemp.open();
+		while (copy(src, currentscan, sch)) { // hashes the current record into a partition
+			// retrieve another partition if the next record hashes to a different partition
+			if (comp.compare(src, currentscan) != 0) {
+				currentscan.close();
+				Constant newVal = src.getVal(fldname);
+				int newOffset = newVal.hashCode() % k;
+				currenttemp = temps.get(newOffset);
+				currentscan = (UpdateScan) currenttemp.open();
+			}
+		}
+		currentscan.close();
+		return temps;
+	}
+
+	private Map<Integer, TempTable> initializePartitions(Transaction tx, Schema sch, int k) {
+		Map<Integer, TempTable> result = new HashMap<>();
+		IntStream.range(0, k).forEach(i -> result.put(i, new TempTable(tx, sch)));
+		return result;
+	}
+
+	private boolean copy(Scan src, UpdateScan dest, Schema sch) {
+		dest.insert();
+		for (String fldname : sch.fields())
+			dest.setVal(fldname, src.getVal(fldname));
+		return src.next();
 	}
 
 	/**
 	 * Prints the plan that is being used.
 	 */
 	public void printPlan() {
-		System.out.println("Hash join plan used on " + fldname1 + opr + fldname2);
+		System.out.println("Hash join plan used on " + fldname1 + opr + fldname2 + " (" + k + " partitions created)");
 	}
 }
--- SimpleDBEngine/src/simpledb/materialize/HashJoinScan.java
+++ SimpleDBEngine/src/simpledb/materialize/HashJoinScan.java
@@ -8,14 +8,18 @@ import java.util.stream.IntStream;
 /**
  * The Scan class for the <i>hashjoin</i> operator.
  * 
- * @author Moon Geonsik
  */
 public class HashJoinScan implements Scan {
 	private Scan s1, s2;
 	private String fldname1, fldname2;
+	private Map<Integer, TempTable> part1, part2;
+	private int k;
+	private Map<Integer, ArrayList<Scan>> hashTables;
+	private HashComparator comp;
+
 	private String opr;
-	private int numPart;
-	private Map<Integer, ArrayList<Constant>> hashTables;
+	
+	private int partId = 0;
 
 	/**
 	 * Create a hashjoin scan having the two underlying scans.
@@ -25,38 +29,42 @@ public class HashJoinScan implements Scan {
 	 * @param fldname1 the LHS join field
 	 * @param fldname2 the RHS join field
 	 * @param opr      the relational operator between join fields
-	 * @param numPart  the number of partitions
+	 * @param k        the number of partitions
 	 */
-	public HashJoinScan(Scan s1, Scan s2, String fldname1, String fldname2, String opr, int numPart) {
+	public HashJoinScan(Scan s1, Scan s2, String fldname1, String fldname2, Map<Integer, TempTable> part1,
+			Map<Integer, TempTable> part2, int k, String opr) {
 		this.s1 = s1;
 		this.s2 = s2;
 		this.fldname1 = fldname1;
 		this.fldname2 = fldname2;
+		this.part1 = part1;
+		this.part2 = part2;
+		this.k = k;
+		this.comp = new HashComparator(fldname1, fldname2, k);
 		this.opr = opr;
-		this.numPart = numPart;
+		s1 = (UpdateScan) part1.get(partId).open();
+		s2 = (UpdateScan) part2.get(partId).open();
 		initializeHashTables();
 		beforeFirst();
 	}
 
 	private void initializeHashTables() {
 		this.hashTables = new HashMap<>();
-		IntStream.range(0, numPart).forEach(i -> hashTables.put(i, new ArrayList<Constant>()));
+		IntStream.range(0, (this.k - 1)).forEach(i -> hashTables.put(i, new ArrayList<Scan>()));
 		return;
 	}
 
 	/**
-	 * Position the scan before its first record. In particular, the LHS scan is
-	 * positioned at its first record, and the RHS scan is positioned before its
-	 * first record.
+	 * Position the scan before its first record. In particular, the LHS scan and
+	 * the RHS scan is positioned before its first record.
 	 * 
 	 * @see simpledb.query.Scan#beforeFirst()
 	 */
 	public void beforeFirst() {
 		s1.beforeFirst();
-		s1.next();
 		s2.beforeFirst();
 	}
-	
+
 	/**
 	 * Move the scan to the next record. The method moves to the next RHS record, if
 	 * possible. Otherwise, it moves to the next LHS record and the first RHS
@@ -65,14 +73,25 @@ public class HashJoinScan implements Scan {
 	 * @see simpledb.query.Scan#next()
 	 */
 	public boolean next() {
-		do {
-			while (s2.next()) {
-				if (OprComparator.compare(s1.getVal(fldname1), s2.getVal(fldname2), opr)) {
-					return true;
+//		populate the hashTables
+//		while (s1.next()) {
+//			Constant val = s1.getVal(fldname1);
+//			int offset = val.hashCode() & (k - 1);
+//			this.hashTables.get(offset).add(s1);
+//		}
+		while (partId <= k) {
+			do {
+				while (s2.next()) {
+					if (comp.compare(s1, s2) == 0) {
+						return true;
+					}
 				}
-			}
-			s2.beforeFirst();
-		} while (s1.next());
+				s2.beforeFirst();
+			} while (s1.next());
+			partId ++;
+			s1 = (UpdateScan) part1.get(partId).open();
+			s2 = (UpdateScan) part2.get(partId).open();
+		}
 		return false;
 	}
 
@@ -84,5 +103,66 @@ public class HashJoinScan implements Scan {
 	public void close() {
 		s1.close();
 		s2.close();
+	}
+
+	/**
+	 * Return the integer value of the specified field. The value is obtained from
+	 * whichever scan contains the field.
+	 * 
+	 * @see simpledb.query.Scan#getInt(java.lang.String)
+	 */
+	public int getInt(String fldname) {
+		if (s1.hasField(fldname))
+			return s1.getInt(fldname);
+		else
+			return s2.getInt(fldname);
+	}
+
+	/**
+	 * Return the float value of the specified field. The value is obtained from
+	 * whichever scan contains the field.
+	 * 
+	 * @see simpledb.query.Scan#getFloat(java.lang.String)
+	 */
+	public float getFloat(String fldname) {
+		if (s1.hasField(fldname))
+			return s1.getFloat(fldname);
+		else
+			return s2.getFloat(fldname);
+	}
+
+	/**
+	 * Return the string value of the specified field. The value is obtained from
+	 * whichever scan contains the field.
+	 * 
+	 * @see simpledb.query.Scan#getString(java.lang.String)
+	 */
+	public String getString(String fldname) {
+		if (s1.hasField(fldname))
+			return s1.getString(fldname);
+		else
+			return s2.getString(fldname);
+	}
+
+	/**
+	 * Return the value of the specified field. The value is obtained from whichever
+	 * scan contains the field.
+	 * 
+	 * @see simpledb.query.Scan#getVal(java.lang.String)
+	 */
+	public Constant getVal(String fldname) {
+		if (s1.hasField(fldname))
+			return s1.getVal(fldname);
+		else
+			return s2.getVal(fldname);
+	}
+
+	/**
+	 * Return true if the specified field is in either of the underlying scans.
+	 * 
+	 * @see simpledb.query.Scan#hasField(java.lang.String)
+	 */
+	public boolean hasField(String fldname) {
+		return s1.hasField(fldname) || s2.hasField(fldname);
 	}
 }
