7835ba726be65f900014423976e1e4283dc21743
Wincent Tjoi <wincenttsjoi@gmail.com>
2022-03-16 19:34:00 +0800

    Update tests

--- SimpleDBClients/test/Lab1Test.java
+++ SimpleDBClients/test/Lab1Test.java
@@ -192,42 +192,6 @@ public class Lab1Test {
 //        sname  
 //        ------
     	  
-    	  Test.doTest(stmt, "select sid, sname, majorid from student where majorid > 10 and sname != 'bob'");
-// TEST18
-//    	    sid      sname majorid
-//    	    --------------------------
-//    	          8        pat      20
-//    	          6        kim      20
-//    	          4        sue      20
-//    	          2        amy      20
-//    	          7        art      30
-    	  
-    	  Test.doTest(stmt, "select sname, sid, majorid from student where majorid > 10 and majorid < 30");
-// TEST19
-//          sname    sid majorid
-//          --------------------------
-//                  pat      8      20
-//                  kim      6      20
-//                  sue      4      20
-//                  amy      2      20
-    	  
-    	  Test.doTest(stmt, "select sname, sid, majorid from student where majorid > 10 and majorid > 20");
-// TEST20
-//          sname    sid majorid
-//          --------------------------
-//                  art      7      30
-//                  bob      5      30
-    	  
-    	  Test.doTest(stmt, "select sname, sid, majorid from student where 10 < majorid and majorid > 10");
-// TEST21
-//          sname    sid majorid
-//          --------------------------
-//                  pat      8      20
-//                  kim      6      20
-//                  sue      4      20
-//                  amy      2      20
-//                  art      7      30
-//                  bob      5      30
     
 
       }
