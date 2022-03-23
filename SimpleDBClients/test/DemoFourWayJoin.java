af8ad7235a705589c1b72ea39936484a5109b621
kelvinvin <kelvinw0ng@hotmail.com>
2022-03-23 15:28:19 +0800

    Update DemoFourWayJoin.java
    Remove tests 1 and 2

--- SimpleDBClients/test/DemoFourWayJoin.java
+++ SimpleDBClients/test/DemoFourWayJoin.java
@@ -26,92 +26,15 @@ public class DemoFourWayJoin {
 //    	  Test.doQuery(stmt, "select CId, Title, DeptId from course");
     	  
     	  String query;
-    	  // TEST1: Four Way table queries, showing different sname and prof for each course subject
-    	  query = "select sname, majorid, did, deptid, dname, title, cid, courseid, prof "
-        	  		+ "from student, dept, course, section "
-        	  		+ "where majorid = did and did = deptid and cid = courseid order by dname asc, title asc";
-    	  Test.doTest(stmt, query);
-    	  Test.doJoinAlgoTest(stmt, query);
-//          sname majorid    did deptid                     dname                          title    cid courseid                      prof
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-//   Darci Hodson     100    100    100                Accounting              Corporate Finance    102      102              Moon Geonsik
-//   Darci Hodson     100    100    100                Accounting              Corporate Finance    102      102               Sheikh Rowe
-//   Darci Hodson     100    100    100                Accounting              Corporate Finance    102      102              Amara Tanner
-//    Sayed Marsh     100    100    100                Accounting              Corporate Finance    102      102              Moon Geonsik
-//    Sayed Marsh     100    100    100                Accounting              Corporate Finance    102      102               Sheikh Rowe
-//    Sayed Marsh     100    100    100                Accounting              Corporate Finance    102      102              Amara Tanner
-// Kamile Wilkins     430    430    430      Chemical Engineering            Polymer Engineering    462      462             Kelise Berger
-//  Tevin Wallace     430    430    430      Chemical Engineering            Polymer Engineering    462      462             Kelise Berger
-//      Damon Kim     430    430    430      Chemical Engineering            Polymer Engineering    462      462             Kelise Berger
-// Kamile Wilkins     430    430    430      Chemical Engineering                Process Control    472      472           Garfield Hebert
-// Kamile Wilkins     430    430    430      Chemical Engineering                Process Control    472      472                   Cole Li
-//  Tevin Wallace     430    430    430      Chemical Engineering                Process Control    472      472           Garfield Hebert
-//  Tevin Wallace     430    430    430      Chemical Engineering                Process Control    472      472                   Cole Li
-//      Damon Kim     430    430    430      Chemical Engineering                Process Control    472      472           Garfield Hebert
-//      Damon Kim     430    430    430      Chemical Engineering                Process Control    472      472                   Cole Li
-//Layla-Mae Barker     240    240    240          Computer Science              Basic Programming    242      242           Zakariyah Power
-//Layla-Mae Barker     240    240    240          Computer Science              Basic Programming    242      242              Wincent Tjoi
-//      Marwah Yu     250    250    250          Computer Science                      Databases    252      252               Chase Wyatt
-//      Marwah Yu     250    250    250          Computer Science                      Databases    252      252                 Ali Heath
-//      Marwah Yu     250    250    250          Computer Science                      Databases    252      252            Ritchie Cairns
-//   Jemima Davey     250    250    250          Computer Science                      Databases    252      252               Chase Wyatt
-//   Jemima Davey     250    250    250          Computer Science                      Databases    252      252                 Ali Heath
-//   Jemima Davey     250    250    250          Computer Science                      Databases    252      252            Ritchie Cairns
-//      Marwah Yu     250    250    250          Computer Science                      Databases    252      252                Rohan Drew
-//   Jemima Davey     250    250    250          Computer Science                      Databases    252      252                Rohan Drew
-// Shiloh Hartman     310    310    310         Industrial Design                     IDE Design    402      402            Nellie Wiggins
-//Lilly-Ann Joyce      10     10     10                       Law                   Criminal Law     12       12            Korban Fleming
-//Lilly-Ann Joyce      10     10     10                       Law                   Criminal Law     12       12            Gracey Peacock
-// Scott Goulding      10     10     10                       Law                   Criminal Law     12       12            Korban Fleming
-// Scott Goulding      10     10     10                       Law                   Criminal Law     12       12            Gracey Peacock
-//    Otis Devlin      50     50     50                       Law              Equity and Trusts     52       52                Jibril Ali
-//    Otis Devlin      50     50     50                       Law              Equity and Trusts     52       52               Zaina Myers
-//    Otis Devlin      50     50     50                       Law              Equity and Trusts     52       52             Rosina Lucero
-// Callan Derrick      30     30     30                       Law            The Law of Contract     32       32             Layan Bonilla
-//  Imaad Wickens      20     20     20                       Law                   law of Torts     22       22               Alisa David
-//Darcie-Mae Winters      20     20     20                       Law                   law of Torts     22       22               Alisa David
-//  Huzaifa Wiley      20     20     20                       Law                   law of Torts     22       22               Alisa David
-//      Rhia Bird     110    110    110                     Music             Introductory Music    112      112               Dina Warner
-//      Rhia Bird     110    110    110                     Music             Introductory Music    112      112           Beatriz Shields
-//Cerys Armstrong     110    110    110                     Music             Introductory Music    112      112               Dina Warner
-//Cerys Armstrong     110    110    110                     Music             Introductory Music    112      112           Beatriz Shields
-//   Rania Alcock     140    140    140                     Music                Music and Media    142      142             Beatrix Oneal
-//      Elif Mayo     200    200    200               Real Estate   Land Use and Property Rights    202      202             Charlie Dixon
-//  Rohaan Keenan     200    200    200               Real Estate   Land Use and Property Rights    202      202             Charlie Dixon
-// Amiya Caldwell     200    200    200               Real Estate   Land Use and Property Rights    202      202             Charlie Dixon
-//   Amiee Oliver     180    180    180               Real Estate                       RE Taxes    182      182              Ifan Squires
 
-    	  // TEST2: Four Way table queries, with distinct and double order by
-    	  query = "select distinct majorid, did, deptid, dname, title, cid, courseid "
-      	  		+ "from student, dept, course, section "
-      	  		+ "where majorid = did and did = deptid and cid = courseid order by dname, title";
-//    	  Test.doTest(stmt, query);
-//    	  Test.doJoinAlgoTest(stmt, query);
-    	  //    	  majorid    did deptid                     dname                          title    cid courseid
-//    	  -----------------------------------------------------------------------------------------------
-//    	       100    100    100                Accounting              Corporate Finance    102      102
-//    	       430    430    430      Chemical Engineering            Polymer Engineering    462      462
-//    	       430    430    430      Chemical Engineering                Process Control    472      472
-//    	       240    240    240          Computer Science              Basic Programming    242      242
-//    	       250    250    250          Computer Science                      Databases    252      252
-//    	       310    310    310         Industrial Design                     IDE Design    402      402
-//    	        10     10     10                       Law                   Criminal Law     12       12
-//    	        50     50     50                       Law              Equity and Trusts     52       52
-//    	        30     30     30                       Law            The Law of Contract     32       32
-//    	        20     20     20                       Law                   law of Torts     22       22
-//    	       110    110    110                     Music             Introductory Music    112      112
-//    	       140    140    140                     Music                Music and Media    142      142
-//    	       200    200    200               Real Estate   Land Use and Property Rights    202      202
-//    	       180    180    180               Real Estate                       RE Taxes    182      182
-
-    	  // TEST3: Four Way table queries, with join order to showcase all successful indexjoin. 
+    	  // TEST1: Four Way table queries, with join order to showcase all successful indexjoin. 
     	  // 		Also note the yearOffered = gradyear isn't duplicated in select
     	  query = "select grade, studentid, sid, title, courseid, cid, majorid, yearoffered, gradyear "
     	  		+ "from enroll, student, section, course "
         	  	+ "where grade = 'A-' and studentid = sid and courseid > majorid and courseid = cid and yearOffered = gradyear "
           	  	+ "order by title";
-//    	  Test.doTest(stmt, query);
-//    	  Test.doJoinAlgoTest(stmt, query);
+    	  Test.doTest(stmt, query);
+    	  Test.doJoinAlgoTest(stmt, query);
     	  //    	  grade studentid    sid                          title courseid    cid majorid yearoffered gradyear
 //    	  ---------------------------------------------------------------------------------------------------
 //    	      A-        14     14              Basic Programming      242    242      40        2019     2019
@@ -151,13 +74,13 @@ public class DemoFourWayJoin {
 //    	      A-        45     45                 Thermodynamics      512    512     460        2016     2016
     	  
     	  
-    	  // TEST4: Same as Test 3, but with inequality conditions (check indexjoin, nestedjoin, mergejoin)
+    	  // TEST2: Same as Test 1, but with inequality conditions (check indexjoin, nestedjoin, mergejoin)
     	  query = "select grade, studentid, sid, title, courseid, cid, majorid, yearoffered, gradyear "
     			+ "from enroll, student, section, course "
       	  		+ "where grade = 'A-' and studentid = sid and courseid > majorid and courseid = cid and yearOffered < gradyear and gradYear = 2019 "
       	  		+ "order by title";
-//    	  Test.doTest(stmt, query);
-//    	  Test.doJoinAlgoTest(stmt, query);
+    	  Test.doTest(stmt, query);
+    	  Test.doJoinAlgoTest(stmt, query);
     	  
 //    	  grade studentid    sid                          title courseid    cid majorid yearoffered gradyear
 //    	  ---------------------------------------------------------------------------------------------------
@@ -191,13 +114,13 @@ public class DemoFourWayJoin {
 //    	      A-        24     24                 Thermodynamics      512    512     350        2016     2019
 //    	      A-        14     14                 Thermodynamics      512    512      40        2016     2019
     	  
-    	  // TEST5: Four Way table queries, with group by and order by
+    	  // TEST3: Four Way table queries, with group by and order by
     	  query = "select title, count(sid) from enroll, student, section, course "
         	  		+ "where grade = 'A-' and studentid = sid and courseid > majorid and courseid = cid and yearOffered = gradyear "
           	  		+ "group by title "
         	  		+ "order by title";
-//    	  Test.doTest(stmt, query);
-//    	  Test.doJoinAlgoTest(stmt, query);    	  
+    	  Test.doTest(stmt, query);
+    	  Test.doJoinAlgoTest(stmt, query);    	  
 //			title countofsid
 //			------------------------------------------
 //			Basic Programming          1
